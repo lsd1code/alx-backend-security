18d595b6ccff5d18ffd2a9606f5cfa62a74c9717
lesedi thibile <lesedi.xys11@gmail.com>
2025-09-03 20:54:53 +0200

    CHORE: add request log model

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class RequestLog(models.Model):
+    ip_address = models.CharField(max_length=32, null=False)
+    path = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return 'f{self.id}'
